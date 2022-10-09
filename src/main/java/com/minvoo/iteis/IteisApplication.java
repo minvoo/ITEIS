4624f505398101348dc1859acd897fcfa31fae17
Mariusz Maciejewski <maciejewski_mariusz@wp.pl>
2022-10-09 23:56:37 +0200

    initial commit

---
+++ src/main/java/com/minvoo/iteis/IteisApplication.java
@@ -0,0 +1,13 @@
+package com.minvoo.iteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minvoo/iteis/IteisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minvoo.iteis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
