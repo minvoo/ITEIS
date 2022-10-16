50656d8e92b9bea7f78d787fedea435af484b0d2
Mariusz Maciejewski <maciejewski_mariusz@wp.pl>
2022-10-16 12:26:13 +0200

    admin can't change it's own role.

--- src/main/java/com/minvoo/iteis/controller/AdminWebController.java
+++ src/main/java/com/minvoo/iteis/controller/AdminWebController.java
@@ -1,7 +1,11 @@
 package com.minvoo.iteis.controller;
 
+import com.minvoo.iteis.entity.Employee;
 import com.minvoo.iteis.service.EmployeeService;
+import com.minvoo.iteis.utils.CurrentUserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +19,17 @@ import static com.minvoo.iteis.common.PageMappingInfo.ADMIN_WEB_ROLE_ID_PATH;
 public class AdminWebController {
 
     @Autowired
+    private CurrentUserUtils currentUserUtils;
+    @Autowired
     private EmployeeService employeeService;
 
     @PostMapping(ADMIN_WEB_ROLE_ID_PATH)
     public String changeUserRole(@PathVariable("id") Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Employee currentUser = currentUserUtils.getCurrentUser(authentication);
+        if (currentUser.getId() != id) {
         employeeService.changeRole(id);
+        }
         return "redirect:/employees/list";
     }
 }
